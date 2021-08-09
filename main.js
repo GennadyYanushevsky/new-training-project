0572c762e36b51517ce7cb8aa70616d0a1a06a47
Gennady <example@mail.com>
2021-08-09 23:12:47 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+"use strict";
+
+
+//alert('hello');
+
+//const result = confirm("Слышь, ущербный, ты в курсе, что я к тебе обращаюсь?)");
+
+//console.log(result);
+
+//const answer = prompt("утырок, тебе есть 18?", "");
+//console.log(answer + 5);
+
+//const answers = [];
+
+//answers [0] = prompt('ушлёпок, как тебя звать то?', ''); 
+//answers [1] = prompt('а фамилия у ушлёпка есть?', ''); 
+//answers [2] = prompt('а сколько годиков ушлёпку натикало?)', ''); 
+
+//console.log(typeof(answers));
+//console.log(typeof(null));
+
+
+alert( 4 + "$" + 5);
